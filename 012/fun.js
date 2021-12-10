ebe8736e3f3e586248068aa6d28f5e33449fe00e
A-Kija <arvydas.kijakauskas@gmail.com>
2021-12-10 21:25:28 +0200

    fun 2

--- 012/fun.js
+++ 012/fun.js
@@ -5,6 +5,32 @@ function sayHello(name, count) {
     }
 }
 
+function sum(a, b) {
+    console.log(a + b);
+}
+
+function calculator(a, b, c) {
+    console.log(eval(a + c + b));
+}
+
+function calculatorIf(a, b, c) {
+    if ('-' == c) {
+        console.log(a - b);
+    } else if ('+' == c) {
+        console.log(a + b);
+    } else if ('*' == c) {
+        console.log(a * b);
+    } else if ('/' == c) {
+        console.log(a / b);
+    } else {
+        console.log('error');
+    }
+
+}
+
+
+
+
 console.log('Have FUN!');
 
 const times = 25;
@@ -13,3 +39,10 @@ const times = 25;
 sayHello('Martyna', 1);
 sayHello('Martynai', times);
 sayHello('Bobikai', 10);
+
+
+sum(6, 6);
+sum(18, 18);
+sum(22, 0);
+
+calculatorIf(5, 8, '/');
