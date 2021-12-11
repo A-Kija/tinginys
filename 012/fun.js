e7c9a2a0f9d4e645e0213a3808df02f33ecc3c2e
A-Kija <arvydas.kijakauskas@gmail.com>
2021-12-11 20:53:33 +0200

    baras

--- 012/fun.js
+++ 012/fun.js
@@ -1,3 +1,10 @@
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
 //1. Deklaracija
 function sayHello(name, count) {
     for (let i = 0; i < count; i++) {
@@ -6,7 +13,8 @@ function sayHello(name, count) {
 }
 
 function sum(a, b) {
-    console.log(a + b);
+    return a + b;
+    console.log('BLA BLA');
 }
 
 function calculator(a, b, c) {
@@ -15,31 +23,32 @@ function calculator(a, b, c) {
 
 function calculatorIf(a, b, c) {
     if ('-' == c) {
-        console.log(a - b);
-    } else if ('+' == c) {
-        console.log(a + b);
-    } else if ('*' == c) {
-        console.log(a * b);
-    } else if ('/' == c) {
-        console.log(a / b);
-    } else {
-        console.log('error');
+        return a - b;
     }
+    if ('+' == c) {
+        return a + b;
+    }
+    if ('*' == c) {
+        return a * b;
+    }
+    if ('/' == c) {
+        return a / b;
+    }
+    return 'error';
 }
 
 function calculatorSw(a, b, c) {
     switch (c) {
         case '-':
-            console.log(a - b);
-            break;
+            return a - b;
         case '+':
-            console.log(a + b);
+            return a + b;
         case '*':
-            console.log(a * b);
+            return a * b;
         case '/':
-            console.log(a / b);
+            return a / b;
         default:
-            console.log('error');
+            return 'error';
     }
 }
 
@@ -51,13 +60,80 @@ console.log('Have FUN!');
 const times = 25;
 
 //2. Iškvietimas
-sayHello('Martyna', 1);
-sayHello('Martynai', times);
-sayHello('Bobikai', 10);
+// sayHello('Martyna', 1);
+// sayHello('Martynai', times);
+// sayHello('Bobikai', 10);
 
 
-sum(6, 6);
-sum(18, 18);
+let suma1 = sum(6, 7);
+console.log(suma1);
+console.log(sum(18, 18));
 sum(22, 0);
 
-calculatorSw(5, 8, '+');
+console.log('ja ja ===>', calculatorSw(5, 8, '-'));
+
+
+function add3(a, b) {
+    return [a + 3, b + 3];
+}
+
+// console.log('masyvas ===>', add3(5, 7));
+
+let [k1, k2] = add3(10, 20);
+
+console.log(k1, k2);
+
+function xu() {
+    const masyvas = [];
+    masyvas.push('X');
+    masyvas.push('U');
+    return masyvas;
+}
+let [l1, l2] = xu();
+console.log(l1, l2);
+
+
+function rgb(c) {
+    // const m = ['Red', 'Green', 'Blue'];
+    // m.splice(m.indexOf(c), 1);
+    // return m;
+    return ['Red', 'Green', 'Blue'].filter(color => c != color);
+}
+let [color1, color2] = rgb('Blue');
+console.log(color1, color2);
+
+function neAnonimas() {
+    console.log('Ura');
+}
+
+const anonimas = function() {
+    console.log('Valio');
+}
+
+anonimas();
+neAnonimas();
+
+const baras = {
+    alus: 'Džekas',
+    kedes: rand(5, 20),
+    daina: null // <=== nebūtina
+};
+
+baras.daina = function() {
+    // console.log('2 gaideliai, 2 gaideliai...');
+}
+
+baras.kdd = function() {
+    console.log('Šiame bare yra: ' + this.kedes + ' kedžių');
+}
+
+const baras2 = {...baras }
+
+baras2.kedes = 1000;
+
+
+baras.kdd();
+baras2.kdd();
+
+
+console.log(baras, baras2);
