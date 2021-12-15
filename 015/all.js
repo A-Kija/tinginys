c808aab0266d4179ff91c32be7d992e0f2da519a
A-Kija <arvydas.kijakauskas@gmail.com>
2021-12-15 21:07:13 +0200

    spalvos ir skaiciai

--- 015/all.js
+++ 015/all.js
@@ -6,10 +6,25 @@ const rand = (min, max) => {
 }
 console.log(i);
 
+const data = [
+    { digit: '0000', color: 'black' },
+    { digit: '0000', color: 'black' },
+    { digit: '0000', color: 'black' },
+    { digit: '0000', color: 'black' },
+    { digit: '0000', color: 'black' },
+    { digit: '0000', color: 'black' },
+];
+
 
 setInterval(() => {
+    data.unshift({
+        digit: rand(1000, 9999),
+        color: '#' + Math.floor(Math.random() * 16777215).toString(16)
+    });
+    data.pop();
     for (t = 0; t < i.length; t++) {
-        i[t].style.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
-        i[t].innerText = rand(1000, 9999);
+        // i[t].style.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+        i[t].innerText = data[t].digit;
+        i[t].style.color = data[t].color;
     }
 }, 1000);
