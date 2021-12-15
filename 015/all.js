fdfd8b9462d023b9a7b4fa53f8e243e26a4cfa0b
A-Kija <arvydas.kijakauskas@gmail.com>
2021-12-15 20:02:14 +0200

    kodas

--- 015/all.js
+++ 015/all.js
@@ -1,9 +1,15 @@
 const i = document.querySelectorAll('i');
-
+const rand = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 console.log(i);
 
 
-
-for (t = 0; t < i.length; t++) {
-    i[t].style.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
-}
+setInterval(() => {
+    for (t = 0; t < i.length; t++) {
+        i[t].style.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+        i[t].innerText = rand(1000, 9999);
+    }
+}, 1000);
