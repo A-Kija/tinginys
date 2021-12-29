35f701021c75067e4f32383cf64c5f0a2cb74e9d
A-Kija <arvydas.kijakauskas@gmail.com>
2021-12-29 19:19:14 +0200

    all data

--- 023/app.js
+++ 023/app.js
@@ -50,4 +50,44 @@ radios.forEach(r => {
     })
 })
 
-//CHECKBOX
+//CHECKBOX 1
+
+const buttonCh = document.querySelector('button#ch');
+const chOne = document.querySelector('input#one');
+
+buttonCh.addEventListener('click', () => {
+    const myCh = chOne.checked ? 'YES' : 'NOP';
+    console.log('CLICK CH => ', myCh);
+});
+
+chOne.addEventListener('change', () => {
+    const myCh = chOne.checked ? 'YES' : 'NOP';
+    console.log('CH => ', myCh);
+});
+
+//CHECKBOXES
+
+const buttonChs = document.querySelector('button#chs');
+const allIWant = document.querySelectorAll('input.manyCh');
+
+buttonChs.addEventListener('click', () => {
+    const myCh = [];
+    allIWant.forEach(w => {
+        if (w.checked) {
+            myCh.push(w.value)
+        }
+    })
+    console.log('All I WANT BUTTON => ', myCh);
+});
+
+allIWant.forEach(wi => {
+    wi.addEventListener('change', () => {
+        const myCh = [];
+        allIWant.forEach(w => {
+            if (w.checked) {
+                myCh.push(w.value)
+            }
+        })
+        console.log('All I WANT BUTTON => ', myCh);
+    })
+});
