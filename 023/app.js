cb73fa964476585bff714d641f91d54e7253f0ff
A-Kija <arvydas.kijakauskas@gmail.com>
2021-12-29 18:57:59 +0200

    data

--- 023/app.js
+++ 023/app.js
@@ -26,4 +26,28 @@ buttonSelected.addEventListener('click', () => {
 select.addEventListener('change', () => {
     const mySelection = select.value;
     console.log('CHANGE => ', mySelection);
+});
+
+
+// RADIO
+const buttonRadio = document.querySelector('button#radio');
+const radios = document.querySelectorAll('[name=fm]');
+
+buttonRadio.addEventListener('click', () => {
+    let myRadio;
+    radios.forEach(r => {
+        if (r.checked) {
+            myRadio = r.value;
+        }
+    });
+    console.log('CLICK RADIO => ', myRadio);
+});
+
+radios.forEach(r => {
+    r.addEventListener('change', () => {
+        let myRadio = r.value;
+        console.log('CLICK RADIO => ', myRadio);
+    })
 })
+
+//CHECKBOX
