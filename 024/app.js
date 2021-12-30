a22c4b1e2d759b015802d536edae48b81c593904
A-Kija <arvydas.kijakauskas@gmail.com>
2021-12-30 20:40:23 +0200

    render function

--- 024/app.js
+++ 024/app.js
@@ -1,52 +1,50 @@
 console.log('page loaded');
 const ul = document.querySelector('ul');
+let data = [];
+
+const render = () => {
+    ul.innerHTML = '';
+    data.forEach(element => {
+        const li = document.createElement('li');
+        const html = `
+            <h2>${element.username}</h2>
+            <small style="color:gray;">${element.email}</small>
+        `;
+        li.innerHTML = html;
+        ul.appendChild(li);
+    });
+}
 
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(strukura => {
         console.log(strukura);
-        // sort here <----
-        strukura.sort((a, b) => {
-            if (a.username > b.username) {
-                return 1;
-            } else if (a.username < b.username) {
-                return -1;
-            } else {
-                return 0;
-            }
-        });
-        strukura.forEach(element => {
-            const li = document.createElement('li');
-            // const userName = document.createTextNode(element.username);
-            // li.appendChild(userName);
-            // const small = document.createElement('div');
-            // small.innerText = element.email;
-            // small.style.fontSize = '9px';
-            // li.appendChild(small);
-            const html = `
-                <h2>${element.username}</h2>
-                <small style="color:gray;">${element.email}</small>
-            `;
-            li.innerHTML = html;
-            ul.appendChild(li);
-        });
-        // tuscia vieta
+        data = strukura;
+        render();
     });
 
-
-const masyvas = ['Pupsis', 'Mupsis', 'Šarikas', 'Pupsis', 'Bobikas'];
-const masyvasSk = [8, 65, 78, 255, 147, 8, 744];
-
-masyvasSk.sort((a, b) => b - a);
-
-masyvas.sort((a, b) => {
-    if (a > b) {
-        return 1;
-    } else if (a < b) {
-        return -1;
-    } else {
-        return 0;
-    }
+document.querySelector('#az').addEventListener('click', () => {
+    data.sort((a, b) => {
+        if (a.username > b.username) {
+            return 1;
+        } else if (a.username < b.username) {
+            return -1;
+        } else {
+            return 0;
+        }
+    });
+    render();
 });
 
-console.log(masyvas, masyvasSk);
+document.querySelector('#za').addEventListener('click', () => {
+    data.sort((a, b) => {
+        if (a.username > b.username) {
+            return -1;
+        } else if (a.username < b.username) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+    render();
+});
