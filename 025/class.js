27ba347d21656bc4851597a497d064630fbf8435
A-Kija <42836152+A-Kija@users.noreply.github.com>
2022-01-03 20:41:29 +0200

    sayNames

--- 025/class.js
+++ 025/class.js
@@ -9,6 +9,11 @@ class Cat {
     addYear = () => {
         this.age++;
     }
+    sayNames = () => {
+        for (let i = 0; i < this.age; i++) {
+            this.bigName();
+        }
+    }
 }
 
 const cat1 = new Cat('Murka', 3);
@@ -17,5 +22,6 @@ const cat3 = new Cat('Ryžius', 7);
 
 cat3.addYear();
 cat2.bigName();
+cat1.sayNames();
 
 console.log(cat1, cat2, cat3);
