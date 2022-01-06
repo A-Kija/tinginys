c6f12ffa931f257df0d743e9ccc0d5ec03f71569
A-Kija <arvydas.kijakauskas@gmail.com>
2022-01-06 21:29:56 +0200

    kodas 2

--- kartojimas/02.js
+++ kartojimas/02.js
@@ -4,12 +4,14 @@ function rand(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const iki = rand(5, 10);
-let skirtingoIlgio = '';
 
-for (let i = 0; i < iki; i++) {
-    skirtingoIlgio += 'X'
+for (let k = 0; k < 11; k++) {
+
+    const iki = rand(5, 10);
+    let skirtingoIlgio = '';
+    for (let i = 0; i < iki; i++) {
+        skirtingoIlgio += 'X'
+    }
+    console.log(skirtingoIlgio);
+
 }
-
-
-console.log(skirtingoIlgio);
