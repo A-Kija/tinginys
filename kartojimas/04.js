8258dd5896093c6690ec6e7a9808fb20cd31c896
A-Kija <arvydas.kijakauskas@gmail.com>
2022-01-08 19:26:54 +0200

    04 1

---
+++ kartojimas/04.js
@@ -0,0 +1,62 @@
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const hello = 'Kitty';
+
+const helloArray = ['K', 'i', 't', 't', 'y'];
+
+for (let i = 0; i < hello.length; i++) {
+    console.log(hello[i]);
+}
+
+for (let i = 0; i < helloArray.length; i++) {
+    console.log(helloArray[i]);
+}
+
+
+const digArray = [5, 5, 10, 10, 105, 105];
+
+
+for (let i = 0; i < digArray.length; i++) {
+    console.log(digArray[i]);
+}
+
+let digSum = 0;
+for (let i = 0; i < digArray.length; i++) {
+    if (digArray[i] > 10) {
+        digSum += digArray[i];
+    }
+}
+
+console.log(digSum);
+
+
+let dig5 = 0;
+let dig5Sum = 0;
+for (let i = 0; i < digArray.length; i++) {
+    if (digArray[i] > 5) {
+        dig5++;
+        dig5Sum += digArray[i];
+    }
+}
+
+console.log(dig5Sum / dig5);
+
+const array = [];
+
+array.push('Mikis');
+array.push('Mausas');
+console.log(array);
+
+// const seas = ['Black Sea', 'Caribbean Sea', 'North Sea', 'Baltic Sea'];
+
+const randArray = [];
+
+for (let i = 0; i < 10; i++) {
+    randArray.push(rand(5, 20));
+}
+
+console.log(randArray);
